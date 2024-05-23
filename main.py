6bfb17249589516daf963f7591d28598c688079d
Sashreek007 <Sashreek.addanki@gmail.com>
2024-05-23 14:06:21 +0530

    edit

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ class AvlNode:
         self.right= None
         self.height=1
 
-def LevelOrderTraversal(rootnode):
-    if not rootnode:
+def levelOrderTraversal(rootNode):
+    if not rootNode:
         return
     else:
-        custom_queue=Queue()
-        custom_queue.enqueue(rootnode)
-        while not (custom_queue.isEmpty()):
-            root = custom_queue.dequeue()
+        customQueue = Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
             print(root.value.data)
             if root.value.left is not None:
-                custom_queue.enqueue(root.value.left)
+                customQueue.enqueue(root.value.left)
             if root.value.right is not None:
-                custom_queue.enqueue(root.value.right)
+                customQueue.enqueue(root.value.right)
 
 def searching(rootnode,target):
     if rootnode.data is None:
@@ -36,10 +36,101 @@ def searching(rootnode,target):
         else:
             return "Not found"
 
+def getHeight(rootnode):
+    if not rootnode:
+        return 0
+    return rootnode.height
+
+def rightRotation(disbalancedNode):
+    newRoot=disbalancedNode.left
+    disbalancedNode.left=newRoot.right
+    newRoot.right=disbalancedNode
+    disbalancedNode.height= 1+ max(getHeight(disbalancedNode.left),getHeight(disbalancedNode.right))
+    newRoot.height= 1+ max(getHeight(newRoot.left),getHeight(newRoot.right)) 
+    return newRoot
+
+def leftRotation(disbalancedNode):
+    newRoot= disbalancedNode.right
+    disbalancedNode.right=newRoot.left
+    newRoot.left=disbalancedNode
+    disbalancedNode.height= 1+ max(getHeight(disbalancedNode.left),getHeight(disbalancedNode.right)) 
+    newRoot.height= 1+ max(getHeight(newRoot.left),getHeight(newRoot.right)) 
+    return newRoot
+
+def getBalance(rootnode):
+    if not rootnode:
+        return 0
+    return getHeight(rootnode.left)-getHeight(rootnode.right)
+
+def insert(rootnode,newnode):
+    if not rootnode:
+        return AvlNode(newnode)
+    if newnode < rootnode.data:
+        rootnode.left=insert(rootnode.left,newnode)
+    else:
+        rootnode.right=insert(rootnode.right,newnode)
     
-avl = AvlNode(None)
-LevelOrderTraversal(avl)
-print(searching(avl,10))
+    rootnode.height=1+ max(getHeight(rootnode.left),getHeight(rootnode.right))
+    balance= getBalance(rootnode)
+    if balance >1 and newnode< rootnode.left.data:
+        return rightRotation(rootnode)
+    if balance >1 and newnode > rootnode.left.data:
+        rootnode.left= leftRotation(rootnode.left)
+        return rightRotation(rootnode)
+    if balance < -1 and newnode > rootnode.right.data:
+        return leftRotation(rootnode)
+    if balance < -1 and newnode < rootnode.right.data:
+        rootnode.right= rightRotation(rootnode.right)
+        return leftRotation(rootnode)
+    return rootnode
+
+
+def minimum_value(rootnode):
+    if rootnode is None or rootnode.left is None:
+        return rootnode
+    return minimum_value(rootnode.left)
+
+def delete_node(rootnode,nodevalue):
+    if not rootnode:
+        return
+    elif nodevalue < rootnode.data:
+        rootnode.left= delete_node(rootnode.left,nodevalue)
+    elif nodevalue> rootnode.data:
+        rootnode.right= delete_node(rootnode.right,nodevalue)
+    else:
+        if rootnode.left is None:
+            temp= rootnode.right
+            rootnode=None
+            return temp
+        if rootnode.right is None:
+            temp = rootnode.left
+            rootnode=None
+            return temp
+        temp = minimum_value(rootnode.right)
+        rootnode.data=temp.data
+        rootnode.right = delete_node(rootnode.right,temp.data)
+        rootnode.height=1+ max(getHeight(rootnode.left),getHeight(rootnode.right))
+        balance= getBalance(rootnode)
+        if balance >1 and nodevalue< rootnode.left.data:
+            return rightRotation(rootnode)
+        if balance >1 and nodevalue > rootnode.left.data:
+            rootnode.left= leftRotation(rootnode.left)
+            return rightRotation(rootnode)
+        if balance < -1 and nodevalue > rootnode.right.data:
+            return leftRotation(rootnode)
+        if balance < -1 and nodevalue < rootnode.right.data:
+            rootnode.right= rightRotation(rootnode.right)
+            return leftRotation(rootnode)
+        return rootnode
+        
+avl = AvlNode(50)
+avl=insert(avl,40)
+avl=insert(avl,65)
+avl=insert(avl,60)
+avl=insert(avl,70)
+avl=insert(avl,75)
+ 
+levelOrderTraversal(avl)
 
 
 
